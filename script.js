9ef15e459b96e8675b58cb866a8d0fce2af19440
Izmyatinskaya Darya <izmatinskaad@gmail.com>
2024-05-14 15:15:00 +0300

    adapt site

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let l1 = document.querySelector('.slider');
+const burger = document.querySelector('.burger');
+const line1 = document.querySelector('.burger img:nth-child(1)');
+const line2 = document.querySelector('.burger img:nth-child(2)');
+const line3 = document.querySelector('.burger img:nth-child(3)');
+const block = document.querySelector('.header-col2');
+let images = document.querySelectorAll('.slider  img');
 let x = -270;
 let c=0;
+burger.addEventListener('click', () => {
+    line3.classList.toggle('active3');
+    line2.classList.toggle('active2');
+    line1.classList.toggle('active');
+    block.classList.toggle('head-active');
+  });
 function move()
 {
     if (window.matchMedia("(max-width: 767px)").matches)
@@ -28,5 +40,8 @@ function move()
         }
     }
 }
+
+
+
     
     
